039fcb94a2f912d164e910d707d30f44b1c11f12
Pablo Sonaglioni <pablosonaglioni@gmail.com>
2021-07-17 14:32:13 -0300

    Objetivo funcionando.

---
+++ gui/niveles.lua
@@ -0,0 +1,19 @@
+local M = {}
+-- El número del plano del nivel se lee en string.
+M["1"] = {
+0,2 ,0,0,0,
+0,0,0,0,0,
+0,0,0,0,0,
+0,0,0,0,0,
+0,0,0,0,0
+}
+M[1] = {}
+M[1]["cuanto_por_cuanto"] = 5
+
+
+
+
+
+
+
+return M
