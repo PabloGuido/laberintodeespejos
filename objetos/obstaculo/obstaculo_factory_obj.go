f42307458c2d438de799972395db0c73b347172b
Pablo Sonaglioni <pablosonaglioni@gmail.com>
2021-07-18 22:02:32 -0300

    Obstáculos funcionando, hay que cambiar la parte visual por un GUI.

---
+++ objetos/obstaculo/obstaculo_factory_obj.go
@@ -0,0 +1,30 @@
+components {
+  id: "obstaculo_factory"
+  component: "/objetos/obstaculo/obstaculo_factory.factory"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "obstaculo_factory_scr"
+  component: "/objetos/obstaculo/obstaculo_factory_scr.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
