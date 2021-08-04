3fb9b64c182de91660aaa9f8b8194c94058a41eb
Pablo Sonaglioni <pablosonaglioni@gmail.com>
2021-08-04 15:20:02 -0300

    En proceso.

---
+++ escenas/loader/loader.go
@@ -0,0 +1,30 @@
+components {
+  id: "loader"
+  component: "/escenas/loader/loader.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "pantalla_inicial"
+  component: "/escenas/pantalla inicial/pantalla_inicial.collectionproxy"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
