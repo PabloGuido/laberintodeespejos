f8d1e1465682e291f436636b3e3eb8c46b1f5b56
Pablo Sonaglioni <pablosonaglioni@gmail.com>
2021-08-13 19:10:21 -0300

    Los sonidos van queriendo. :)

--- escenas/loader/loader.go
+++ escenas/loader/loader.go
@@ -78,3 +78,26 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "proto_cancion_1"
+  type: "sound"
+  data: "sound: \"/escenas/loader/proto_cancion_1.ogg\"\n"
+  "looping: 1\n"
+  "group: \"master\"\n"
+  "gain: 1.0\n"
+  "pan: 0.0\n"
+  "speed: 1.0\n"
+  "loopcount: 0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
