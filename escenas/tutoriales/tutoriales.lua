ea6056973e64d575fb37965f5cd8a080285026bc
Pablo Sonaglioni <pablosonaglioni@gmail.com>
2021-08-15 13:25:53 -0300

    Armando el sistema de tutoriales.

--- escenas/tutoriales/tutoriales.lua
+++ escenas/tutoriales/tutoriales.lua
@@ -1,5 +1,47 @@
+local M = {}
+M["1"] = {}
+M["1"]["hacer_tuto"] = true
+M["1"][1] = "¡Hola! Mi nombre es Delta, y lo que más\nme gusta en el mundo es crear mis propios\ninventos y jugar con mis amigos."
+M["1"]["sonido_1"] = "tutoriales#tuto_1_1"
+M["1"][2] = "hola mundo 2!"
+M["1"]["sonido_2"] = "tutoriales#tuto_1_1"
+M["1"][3] = "hola mundo 3!"
+M["1"]["sonido_3"] = "tutoriales#tuto_1_1"
+
+
+
+-------
+M["2"] = {}
+M["2"][1] = "hola mundo tuto 2!"
+M["2"][2] = "hola mundo tuto 2.2!"
+M["2"][3] = "hola mundo tuto2.3!"
+return M
+
+
+
 -- Tutoriales --
---[[
+--[[ 
+Fabricar los inventos
+
+Crear mis propios inventos y jugar con mis amigos.
+1.1) ¡Hola! Mi nombre es Delta, y lo que más me gusta en el mundo es crear mis propios inventos
+y jugar con mis amigos.
+
+Sabías que si apuntás un laser a un espejo este reobota y sigue su camino?
+Con esa idea en mente hice este juego, dejame explicarte como funciona.
+El objetivo es hacer llegar el laser al objetivo que ves en el centro. Para eso el laser tiene
+que rebotar en los espjos que ves acá arriba.
+Solo arrastralos hasta uno de los casilleros y soltalo para que se quede en el lugar.
+Podes usar el mouse o el dedo si tu pantalla es tactil.
+Podés usar toda la cantidad de espejos que quieras y moverlos, si necesitas borrar alguno solo
+arrastralo por fuera de los casilleros.
+Una última cosa, si necesitas que te de un pista solo tocá en la lupa que ves a la derecha y te 
+voy a iluminar lugares para que econtremos una solución.
+Listo, empecemos, arrastrá los epejos y hacelos girar. Juegmos.
+
+
+Muy bien, ahora el siguiente punto es girar el espejo para que apunte en la dirección correcta.
+Usá las flechas que ves acá arriba, apareceran siempre que tengas seleccionado un espejo.
 1)
 a) Lo primero que pasa es que Delta se presenta brevemente y explica de que trata el juego.
 El juego trata de hacer rebotar el laser en los espejos para hacerlo llegar al objetivo.
@@ -25,19 +67,5 @@ a) Se explica que hay obstáculos y el laser no pasa por sobre ellos. Hay que en
 El laser puede pasar mas de una vez por sobre los switches.
 
 6) Muy bien ahora con todo lo que aprendimos pasemos algunos niveles juntos!
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 ]]
