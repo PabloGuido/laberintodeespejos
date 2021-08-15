d0aa8c13295d2b5eb89934804a16b0762e882ad8
Pablo Sonaglioni <pablosonaglioni@gmail.com>
2021-08-15 01:26:02 -0300

    A punto de agregar a Delta en la escena.

---
+++ escenas/tutoriales/img/tutoriales.lua
@@ -0,0 +1,43 @@
+-- Tutoriales --
+--[[
+1)
+a) Lo primero que pasa es que Delta se presenta brevemente y explica de que trata el juego.
+El juego trata de hacer rebotar el laser en los espejos para hacerlo llegar al objetivo.
+b) Explica el gameplay, primero muestra y explica que los espejos se pueden arrastrar desde el boton y dejar
+en una casilla.
+c) Luego explica que cuendo tengas un espejo seleccionado lo veras el circulo girando a su alrededor y se podrá
+girar usando los botones que aparecen en la pantalla.
+d) Si necesitas borrarlo solo arrastralo por fuera y se borrará.
+e) Pero por ahora arratrá el espejo hasta le casillero y hacelo girar para que el laser llegue hasta el objetivo.
+
+2)
+a) Muy bien, con lo que aprendiste en el nivel anterior colocá espejos en los casilleros y giralos para
+hacerlos llegar hasta el objetivo.
+b) Si necesitas ayuda te puedo dar pistas de donde colocar los espejos, solo tocá en la lupa y te marcaré 
+algunos casillero para que pongas los espejos y puedas girarlos.
+
+3)
+a) Se explica que hay obstáculos y el laser no pasa por sobre ellos. Hay que encontrar el camino.
+
+4) Se explican los switches
+
+5) En este nivel hay mas de un switch, recuerda que el laser tiene que pasar por todos.
+El laser puede pasar mas de una vez por sobre los switches.
+
+6) Muy bien ahora con todo lo que aprendimos pasemos algunos niveles juntos!
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+]]
