95c8c25d49c7d2d93d5751d5161b819905966224
Pablo Sonaglioni <pablosonaglioni@gmail.com>
2021-08-15 17:30:08 -0300

    El tutorial 1 estaría completo, falta viñeta visual.

--- escenas/tutoriales/tutoriales.go
+++ escenas/tutoriales/tutoriales.go
@@ -1,7 +1,214 @@
 embedded_components {
   id: "tuto_1_1"
   type: "sound"
-  data: "sound: \"/escenas/tutoriales/tuto 1/tutorial_test.ogg\"\n"
+  data: "sound: \"/escenas/tutoriales/tuto 1/tuto_1_1.ogg\"\n"
+  "looping: 0\n"
+  "group: \"master\"\n"
+  "gain: 1.0\n"
+  "pan: 0.0\n"
+  "speed: 1.0\n"
+  "loopcount: 0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "tuto_1_2"
+  type: "sound"
+  data: "sound: \"/escenas/tutoriales/tuto 1/tuto_1_2.ogg\"\n"
+  "looping: 0\n"
+  "group: \"master\"\n"
+  "gain: 1.0\n"
+  "pan: 0.0\n"
+  "speed: 1.0\n"
+  "loopcount: 0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "tuto_1_10"
+  type: "sound"
+  data: "sound: \"/escenas/tutoriales/tuto 1/tuto_1_10.ogg\"\n"
+  "looping: 0\n"
+  "group: \"master\"\n"
+  "gain: 1.0\n"
+  "pan: 0.0\n"
+  "speed: 1.0\n"
+  "loopcount: 0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "tuto_1_3"
+  type: "sound"
+  data: "sound: \"/escenas/tutoriales/tuto 1/tuto_1_3.ogg\"\n"
+  "looping: 0\n"
+  "group: \"master\"\n"
+  "gain: 1.0\n"
+  "pan: 0.0\n"
+  "speed: 1.0\n"
+  "loopcount: 0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "tuto_1_4"
+  type: "sound"
+  data: "sound: \"/escenas/tutoriales/tuto 1/tuto_1_4.ogg\"\n"
+  "looping: 0\n"
+  "group: \"master\"\n"
+  "gain: 1.0\n"
+  "pan: 0.0\n"
+  "speed: 1.0\n"
+  "loopcount: 0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "tuto_1_5"
+  type: "sound"
+  data: "sound: \"/escenas/tutoriales/tuto 1/tuto_1_5.ogg\"\n"
+  "looping: 0\n"
+  "group: \"master\"\n"
+  "gain: 1.0\n"
+  "pan: 0.0\n"
+  "speed: 1.0\n"
+  "loopcount: 0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "tuto_1_6"
+  type: "sound"
+  data: "sound: \"/escenas/tutoriales/tuto 1/tuto_1_6.ogg\"\n"
+  "looping: 0\n"
+  "group: \"master\"\n"
+  "gain: 1.0\n"
+  "pan: 0.0\n"
+  "speed: 1.0\n"
+  "loopcount: 0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "tuto_1_7"
+  type: "sound"
+  data: "sound: \"/escenas/tutoriales/tuto 1/tuto_1_7.ogg\"\n"
+  "looping: 0\n"
+  "group: \"master\"\n"
+  "gain: 1.0\n"
+  "pan: 0.0\n"
+  "speed: 1.0\n"
+  "loopcount: 0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "tuto_1_8"
+  type: "sound"
+  data: "sound: \"/escenas/tutoriales/tuto 1/tuto_1_8.ogg\"\n"
+  "looping: 0\n"
+  "group: \"master\"\n"
+  "gain: 1.0\n"
+  "pan: 0.0\n"
+  "speed: 1.0\n"
+  "loopcount: 0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "tuto_1_9"
+  type: "sound"
+  data: "sound: \"/escenas/tutoriales/tuto 1/tuto_1_9.ogg\"\n"
   "looping: 0\n"
   "group: \"master\"\n"
   "gain: 1.0\n"
--- escenas/tutoriales/tutoriales.lua
+++ escenas/tutoriales/tutoriales.lua
@@ -6,36 +6,36 @@ M["1"][1] = "¡Hola! Mi nombre es Delta, y lo que más\nme gusta en el mundo es 
 M["1"]["sonido_1"] = "tutoriales#tuto_1_1"
 
 M["1"][2] = "Estuve armando este juego para compartirlo\ncon vos, dejáme que te explique como\nfunciona."
-M["1"]["sonido_2"] = "tutoriales#tuto_1_1"
+M["1"]["sonido_2"] = "tutoriales#tuto_1_2"
 
 M["1"][3] = "Sabías que si apuntás un laser a un espejo\neste rebota y sigue su camino?"
-M["1"]["sonido_3"] = "tutoriales#tuto_1_1"
+M["1"]["sonido_3"] = "tutoriales#tuto_1_3"
 
-M["1"][4] = "El objetivo es hacer llegar el laser hasta el\nobjetivo celeste que ves en el centro. Para eso\nvamos a usar los espejos que ves arriba de mí."
-M["1"]["sonido_4"] = "tutoriales#tuto_1_1"
+M["1"][4] = "Nuestra meta es hacer llegar el laser hasta el\nobjetivo celeste que ves en el centro. Para eso\nvamos a usar los espejos que ves arriba de mí."
+M["1"]["sonido_4"] = "tutoriales#tuto_1_4"
 
 M["1"][5] = "Solo arrastralos hasta uno de los casilleros\ny soltalos para que se queden en el lugar. Podes\nusar el mouse o el dedo si tu pantalla es táctil."
-M["1"]["sonido_5"] = "tutoriales#tuto_1_1"
+M["1"]["sonido_5"] = "tutoriales#tuto_1_5"
 M["1"]["animacion_5"] = "mover espejo"
 
 M["1"][6] = "El siguiente paso es girar el espejo para que\napunte en la dirección correcta."
-M["1"]["sonido_6"] = "tutoriales#tuto_1_1"
+M["1"]["sonido_6"] = "tutoriales#tuto_1_6"
 M["1"]["animacion_6"] = "mover espejo 2"
 
 M["1"][7] = "Usá las flechas que ves acá arriba, aparecen\nsiempre que tengas seleccionado un espejo."
-M["1"]["sonido_7"] = "tutoriales#tuto_1_1"
+M["1"]["sonido_7"] = "tutoriales#tuto_1_7"
 M["1"]["animacion_7"] = "rotar espejo"
 
-M["1"][8] = "Podés mover y usar la cantidad de espejos que\nquieras, si necesitas borrar alguno solo\narrastralo por fuera de los casilleros."
-M["1"]["sonido_8"] = "tutoriales#tuto_1_1"
+M["1"][8] = "Podés mover y usar la cantidad de espejos que\nquieras, si necesitás borrar alguno solo\narrastralo por fuera de los casilleros."
+M["1"]["sonido_8"] = "tutoriales#tuto_1_8"
 M["1"]["animacion_8"] = "rotar espejo 2"
 
-M["1"][9] = "Una última cosa, si necesitas ayuda solo\ntocá en la lupa que ves a la derecha y te daré\npistas para que resolvamos el nivel."
-M["1"]["sonido_9"] = "tutoriales#tuto_1_1"
+M["1"][9] = "Una última cosa, si necesitás ayuda solo\ntocá en la lupa que ves a la derecha y te daré\npistas para que resolvamos el nivel."
+M["1"]["sonido_9"] = "tutoriales#tuto_1_9"
 M["1"]["animacion_9"] = "rotar espejo 2"
 
-M["1"][10] = "Listo, eso es todo por ahora.\nEmpecemos a jugar, arrastrá los epejos\ny hacelos girar."
-M["1"]["sonido_10"] = "tutoriales#tuto_1_1"
+M["1"][10] = "Listo, eso es todo por ahora.\nEmpecemos a jugar, arrastrá los espejos\ny hacelos girar."
+M["1"]["sonido_10"] = "tutoriales#tuto_1_10"
 M["1"]["animacion_10"] = "rotar espejo 2"
 
 
