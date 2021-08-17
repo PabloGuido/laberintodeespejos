e18b254c21f9dd11a4216480612ef4385c48f6c4
Pablo Sonaglioni <pablosonaglioni@gmail.com>
2021-08-17 03:20:40 -0300

    Las estrellas parecen estar funcionando sin problema, seguir probando un poco mas de todos modos. Pero la cosa ya estaría.

--- gui/niveles.lua
+++ gui/niveles.lua
@@ -3,6 +3,7 @@ local abajo = vmath.vector3(0,0,270)
 local izquierda = vmath.vector3(0,0,180)
 local derecha = vmath.vector3(0,0,0)
 local niveles = false
+local superado = false
 local M = {}
 --[[ Objetos 
 1) Obstáculos
@@ -28,6 +29,7 @@ M[1]["switches"] = 0
 M[1]["habilitado"] = true -- este true para el incio del juego
 M[1]["direccion"] = vmath.vector3(1, 0, 0)
 M[1]["rotacion"] = derecha
+M[1]["superado"] = superado
 
 --2
 M["2"] = {
@@ -40,9 +42,10 @@ M["2"] = {
 M[2] = {}
 M[2]["cuanto_por_cuanto"] = 5
 M[2]["switches"] = 0
-M[2]["habilitado"] = true
+M[2]["habilitado"] = niveles
 M[2]["direccion"] = vmath.vector3(0, -1, 0)
 M[2]["rotacion"] = abajo
+M[2]["superado"] = superado
 
 --3
 M["3"] = {
@@ -55,9 +58,10 @@ M["3"] = {
 M[3] = {}
 M[3]["cuanto_por_cuanto"] = 5
 M[3]["switches"] = 0
-M[3]["habilitado"] = true
+M[3]["habilitado"] = niveles
 M[3]["direccion"] = vmath.vector3(1, 0, 0)
 M[3]["rotacion"] = derecha
+M[3]["superado"] = superado
 
 --4
 M["4"] = {
@@ -70,9 +74,10 @@ M["4"] = {
 M[4] = {}
 M[4]["cuanto_por_cuanto"] = 5
 M[4]["switches"] = 0
-M[4]["habilitado"] = true
+M[4]["habilitado"] = niveles
 M[4]["direccion"] = vmath.vector3(0, 1, 0)
 M[4]["rotacion"] = arriba
+M[4]["superado"] = superado
 
 --5
 M["5"] = {
@@ -88,6 +93,7 @@ M[5]["switches"] = 0
 M[5]["habilitado"] = niveles
 M[5]["direccion"] = vmath.vector3(-1, 0, 0)
 M[5]["rotacion"] = izquierda
+M[5]["superado"] = superado
 
 --6
 M["6"] = {
@@ -103,6 +109,7 @@ M[6]["switches"] = 2
 M[6]["habilitado"] = niveles
 M[6]["direccion"] = vmath.vector3(1, 0, 0)
 M[6]["rotacion"] = derecha
+M[6]["superado"] = superado
 
 --7
 M["7"] = {
@@ -118,6 +125,7 @@ M[7]["switches"] = 3
 M[7]["habilitado"] = niveles
 M[7]["direccion"] = vmath.vector3(0, 1, 0)
 M[7]["rotacion"] = arriba
+M[7]["superado"] = superado
 
 --8
 M["8"] = {
@@ -133,6 +141,7 @@ M[8]["switches"] = 4
 M[8]["habilitado"] = niveles
 M[8]["direccion"] = vmath.vector3(0, 1, 0)
 M[8]["rotacion"] = arriba
+M[8]["superado"] = superado
 
 --9
 M["9"] = {
@@ -148,6 +157,7 @@ M[9]["switches"] = 2
 M[9]["habilitado"] = niveles
 M[9]["direccion"] = vmath.vector3(1, 0, 0)
 M[9]["rotacion"] = derecha
+M[9]["superado"] = superado
 
 --10
 M["10"] = {
@@ -163,5 +173,6 @@ M[10]["switches"] = 3
 M[10]["habilitado"] = niveles
 M[10]["direccion"] = vmath.vector3(0, -1, 0)
 M[10]["rotacion"] = abajo
+M[10]["superado"] = superado
 
 return M
