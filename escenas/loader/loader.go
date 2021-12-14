3923b3c3bd7eb3b9ad2822b58b62b2aeb774ad05
Pablo Sonaglioni <pablosonaglioni@gmail.com>
2021-12-14 13:09:01 -0300

    Sacando sombras.

--- escenas/loader/loader.go
+++ escenas/loader/loader.go
@@ -59,26 +59,6 @@ components {
   }
 }
 embedded_components {
-  id: "sprite"
-  type: "sprite"
-  data: "tile_set: \"/escenas/pantalla inicial/img/pantalla_inicial.atlas\"\n"
-  "default_animation: \"fondo inicio\"\n"
-  "material: \"/builtins/materials/sprite.material\"\n"
-  "blend_mode: BLEND_MODE_ALPHA\n"
-  ""
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
-}
-embedded_components {
   id: "proto_cancion_1"
   type: "sound"
   data: "sound: \"/escenas/loader/proto_cancion_1.ogg\"\n"
