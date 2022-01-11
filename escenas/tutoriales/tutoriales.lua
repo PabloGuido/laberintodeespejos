19bc34c67963665cd010a3fd5a00a9f93551c7f1
Pablo Sonaglioni <pablosonaglioni@gmail.com>
2022-01-11 19:59:28 -0300

    Tutoriales corregidos, faltan los audios.

--- escenas/tutoriales/tutoriales.lua
+++ escenas/tutoriales/tutoriales.lua
@@ -5,20 +5,20 @@ M["1"]["hacer_tuto"] = true
 M["1"][1] = "¡Hola! Mi nombre es María, y lo que más\nme gusta en el mundo es crear mis propios\ninventos y jugar con mis amigos."
 M["1"]["sonido_1"] = "tutoriales#tuto_1_1"
 
-M["1"][2] = "Estuve armando este juego para compartirlo\ncon vos, dejáme que te explique como\nfunciona."
+M["1"][2] = "Estuve armando este juego para compartirlo\ncon vos, dejame que te explique cómo\nfunciona."
 M["1"]["sonido_2"] = "tutoriales#tuto_1_2"
 
-M["1"][3] = "Sabías que si apuntás un laser a un espejo\neste rebota y sigue su camino?"
+M["1"][3] = "¿Sabías que, si apuntás un láser a un espejo,\neste rebota y sigue su camino?"
 M["1"]["sonido_3"] = "tutoriales#tuto_1_3"
 
-M["1"][4] = "Nuestra meta es hacer llegar el laser hasta el\nobjetivo celeste que ves en el centro. Para eso\nvamos a usar los espejos que ves arriba de mí."
+M["1"][4] = "Nuestra meta es hacer llegar el láser hasta el\nobjetivo celeste que ves en el centro. Para eso\nvamos a usar los espejos que ves encima de mí."
 M["1"]["sonido_4"] = "tutoriales#tuto_1_4"
 
-M["1"][5] = "Solo arrastralos hasta uno de los casilleros y\nsoltalos para que se queden en el lugar. Podés\nusar el mouse o el dedo si tu pantalla es táctil."
+M["1"][5] = "Solo arrastralos hasta uno de los casilleros y\nsoltalos para que se queden en el lugar. Podés\nusar el ratón y si tu pantalla es táctil también\npodés usar el dedo."
 M["1"]["sonido_5"] = "tutoriales#tuto_1_5"
 M["1"]["animacion_5"] = "mover espejo"
 
-M["1"][6] = "El siguiente paso es girar el espejo para que\napunte en la dirección correcta."
+M["1"][6] = "El siguiente paso es girar el espejo para que\napunte a la dirección correcta."
 M["1"]["sonido_6"] = "tutoriales#tuto_1_6"
 M["1"]["animacion_6"] = "mover espejo 2"
 
@@ -26,11 +26,11 @@ M["1"][7] = "Tocá sobre el espejo y usá las flechas que\nsalen de él."
 M["1"]["sonido_7"] = "tutoriales#tuto_1_7"
 M["1"]["animacion_7"] = "rotar espejo"
 
-M["1"][8] = "Podés mover y usar la cantidad de espejos\nque quieras, si necesitás borrar alguno solo\narrastralo por fuera de los casilleros."
+M["1"][8] = "Podés mover y usar la cantidad de espejos\nque quieras; si necesitás borrar alguno, solo\narrastralo por fuera de los casilleros."
 M["1"]["sonido_8"] = "tutoriales#tuto_1_8"
 M["1"]["animacion_8"] = "rotar espejo 2"
 
-M["1"][9] = "Una última cosa, si necesitás ayuda solo\ntocá en la lupa que ves a la derecha y te daré\npistas para que resolvamos el nivel."
+M["1"][9] = "Una última cosa: si necesitás ayuda solo\ntocá en la lupa que ves a la derecha y te daré\npistas para que resolvamos el nivel."
 M["1"]["sonido_9"] = "tutoriales#tuto_1_9"
 M["1"]["animacion_9"] = "rotar espejo 2"
 
@@ -43,14 +43,14 @@ M["1"]["animacion_10"] = "rotar espejo 2"
 M["2"] = {}
 M["2"]["hacer_tuto"] = true
 
-M["2"][1] = "Muy bien, usemos lo que aprendimos en el\nnivel anterior para resolver este.\nVamos a necesitar mas de un espejo."
+M["2"][1] = "Muy bien, usemos lo que aprendimos en el\nnivel anterior para resolver este.\nVamos a necesitar más de un espejo."
 M["2"]["sonido_1"] = "tutoriales#tuto_2_1"
 
 -------
 M["3"] = {}
 M["3"]["hacer_tuto"] = true
 
-M["3"][1] = "Mirá, hay algo nuevo en los casilleros.\nEsos son obstátculos, como podés ver\nel laser no rebota en ellos."
+M["3"][1] = "Mirá, hay algo nuevo en los casilleros.\nEsos son obstátculos; como podés ver,\nel láser no rebota en ellos."
 M["3"]["sonido_1"] = "tutoriales#tuto_3_1"
 M["3"][2] = "¡Encontremos el camino hasta nuestro\nobjetivo!"
 M["3"]["sonido_2"] = "tutoriales#tuto_3_2"
@@ -65,16 +65,16 @@ M["5"]["hacer_tuto"] = false
 M["6"] = {}
 M["6"]["hacer_tuto"] = true
 
-M["6"][1] = "Mirá hay un objeto nuevo en los casilleros.\nEsos son interruptores."
+M["6"][1] = "Mirá, hay un objeto nuevo en los casilleros.\nEsos son interruptores."
 M["6"]["sonido_1"] = "tutoriales#tuto_6_1"
 
-M["6"][2] = "Y como podés ver el objetivo tiene\nuna barrera que que no permite al laser\nllegar hasta él."
+M["6"][2] = "Y, como podés ver, el objetivo tiene\nuna barrera que no permite al láser\nllegar hasta él."
 M["6"]["sonido_2"] = "tutoriales#tuto_6_2"
 
-M["6"][3] = "Tenemos que pasar el laser por todos\nlos interruptores para desactivar la barrera."
+M["6"][3] = "Tenemos que pasar el láser por todos\nlos interruptores para desactivar la barrera."
 M["6"]["sonido_3"] = "tutoriales#tuto_6_3"
 
-M["6"][4] = "Vamos, activemos todos los interruptores\ny hagamos llegar el laser hasta nuestro\nobjetivo."
+M["6"][4] = "Vamos, activemos todos los interruptores\ny hagamos llegar el láser hasta nuestro\nobjetivo."
 M["6"]["sonido_4"] = "tutoriales#tuto_6_4"
 
 -------
